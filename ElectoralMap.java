15d3aaf032bfcd9ca850749cdbd2f8fb380f3440
meganminnear18 <31036480+meganminnear18@users.noreply.github.com>
2017-12-13 15:05:38 -0500

    Updated

--- ElectoralMap.java
+++ ElectoralMap.java
@@ -1,11 +1,16 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 import java.awt.Color;
 
 public class ElectoralMap {
 
-    private class Subregion {
+    private static HashMap<String, ArrayList<Subregion>> map = new HashMap<>();
+
+    private static class Subregion {
         String name;
         int[] votes;
         double[] xCoor;
@@ -13,28 +18,25 @@ public class ElectoralMap {
         Color color;
 
 
-        private Subregion() {
-
+        private Subregion(double[] x, double[] y) {
+            name = null;
+            xCoor = x;
+            yCoor = y;
         }
 
+        private void setVotes(int rep, int dem, int ind){
+            votes[1] = rep;
+            votes[2] = dem;
+            votes[3] = ind;
+        }
 
-        public void getXYCoor(Scanner inputObject) throws FileNotFoundException {
-
-                inputObject.nextLine();
-                inputObject.nextLine();
-                int pointsNum = Integer.parseInt(inputObject.nextLine());
-
-                for (int point = 0; point < pointsNum; point++) {
-                    xCoor[point] = inputObject.nextDouble();
-                    yCoor[point] = inputObject.nextDouble();
-                    inputObject.nextLine();
-                }
-                StdDraw.polygon(xCoor, yCoor);
+        private void setColor(Color setCol){
+            color = setCol;
+        }
 
             }
-        
-        public void getVotes()
-        }
+
+
 
 // Reads the region input file and then creates Subregion objects based on # of Subregions in file
 // Assigns the Subregions names from the String[] names
@@ -42,27 +44,79 @@ public class ElectoralMap {
 // getXYCoor draws Subregion
 // calls on getVotes to assign SubRegion votes --> getVotes returns the greatest of the 3 and sets Color based off those
 
-    public void visualize(String region) throws FileNotFoundException {
+    public static void visualize(String region) throws FileNotFoundException {
         File inputFile = new File("input/" + region + ".txt");
-        Scanner inputObject = new Scanner(inputFile);
+        File inputFile2 = new File("input/" + region + ".txt");
+        Scanner inputYear = new Scanner(inputFile2);
 
-        String minCoordinates = inputObject.nextLine();
-        String maxCoordinates = inputObject.nextLine();
+        Scanner inputRegion = new Scanner(inputFile);
 
-        int subRegions = Integer.parseInt(inputObject.nextLine());
+        String minCoordinates = inputRegion.nextLine();
+        String maxCoordinates = inputRegion.nextLine();
+
+        String[] holdFirst = minCoordinates.split("   ");
+        String[] holdSecond = maxCoordinates.split("   ");
+        double[] doubleMinCoordinates = new double[2];
+        double[] doubleMaxCoordinates = new double[2];
+
+        for (int i = 0; i < 2; i++) {
+            doubleMinCoordinates[i] = Double.parseDouble(holdFirst[i]);
+            doubleMaxCoordinates[i] = Double.parseDouble(holdSecond[i]);
+        }
+        int xSize = (int) (doubleMaxCoordinates[0] - doubleMinCoordinates[0]);
+        int ySize = (int) (doubleMaxCoordinates[1] - doubleMinCoordinates[1]);
+
+        StdDraw.setCanvasSize(512 * (xSize / ySize), 512);
+        StdDraw.setXscale(doubleMinCoordinates[0], doubleMaxCoordinates[0]);
+        StdDraw.setYscale(doubleMinCoordinates[1], doubleMaxCoordinates[1]);
+
+
+
+        int subRegions = Integer.parseInt(inputRegion.nextLine());
         String[] subNames = new String[subRegions];
-        inputObject.nextLine();
+        inputRegion.nextLine();
 
-        for (int i = 0; i < subRegions; i++) {
-            Subregion sub = new Subregion();
-            sub.name = subNames[i];
-            sub.getXYCoor(inputObject);
+
+        ArrayList<Subregion> holdSubs = new ArrayList<>();
+
+        String[] reg = new String[subRegions];
+        for(int current = 0; current < subRegions; current++)
+        {
+            subNames[current] = inputRegion.nextLine();
+            reg[current] = inputRegion.nextLine();
+            int pointsNum = Integer.parseInt(inputRegion.nextLine());
+            double[] xCoor = new double[pointsNum];
+            double[] yCoor = new double[pointsNum];
+
+            for(int point = 0; point < pointsNum; point++){
+                xCoor[point] = inputRegion.nextDouble();
+                yCoor[point] = inputRegion.nextDouble();
+                inputRegion.nextLine();
+            }
+            Subregion subObject = new Subregion(xCoor, yCoor);
+            subObject.name = subNames[current];
+            holdSubs.add(subObject);
+
+            inputRegion.nextLine();
+
+            StdDraw.polygon(xCoor, yCoor);
 
         }
 
+        map.put(reg[0], holdSubs);
 
+        inputRegion.close();
 
+        System.out.println("size = " + map.size());
     }
 
-}
+
+    public static void main(String[] args) throws FileNotFoundException{
+        visualize("GA");
+        System.out.println("keySet is : \n" + map.keySet());
+    }
+    }
+
+
+
 
